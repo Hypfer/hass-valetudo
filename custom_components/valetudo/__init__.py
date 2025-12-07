268f74608324452e0d689776cb51b3dd4e7fc571
Sören Beye <github@hypfer.de>
2025-12-07 19:43:44 +0100

    fix: Fix icons not loading

--- custom_components/valetudo/__init__.py
+++ custom_components/valetudo/__init__.py
@@ -11,7 +11,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    if entry.data.get("type") == "icons":
+    if entry.data.get("entry_type") == "icons":
         await async_setup_icons(hass) 
         
     # else tbd
